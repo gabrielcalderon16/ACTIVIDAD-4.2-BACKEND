f4fd54cf4851989c06cb731c1fe6240a7f800c1d
gabrielcalderon16 <calderongabriel1603@gmail.com>
2023-12-04 23:08:22 -0400

    app culminada

--- pwa frontend/src/api/calendarApi.js
+++ pwa frontend/src/api/calendarApi.js
@@ -1,13 +1,7 @@
 import axios from 'axios';
 
-
-import { getEnvVariables } from '../helpers';
-
-const { VITE_API_URL } = getEnvVariables()
-
-
 const calendarApi = axios.create({
-    baseURL: VITE_API_URL
+    baseURL: 'https://back2-qm5y.onrender.com/api'
 });
 
 // Todo: configurar interceptores
